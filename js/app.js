498eb9afa59306fba550bc8b116e297d7f21a72f
fisermichal <michalfiser@seznam.cz>
2021-01-05 17:05:31 +0100

    hamburger responsive dropdown menu

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".dropdown-menu");
+const links = document.querySelectorAll(".dropdown-menu li");
+
+hamburger.addEventListener('click', (event) => {    
+    navLinks.classList.toggle("open");
+    links.forEach( link => {
+        link.classList.toggle("fade");
+    });
+    event.preventDefault();
+});
+      
